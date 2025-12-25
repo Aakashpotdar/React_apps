6c3b5034a76261bae8d9195a7ab824fe7443eb79
aakashpotdar <potdaraakash@gmail.com>
2025-12-25 22:47:21 +0530

    react project

---
+++ reactPprojectusingVite/src/App.jsx
@@ -0,0 +1,13 @@
+import Test from './Test.jsx';
+
+function App() {
+ 
+  return (
+    <>
+      <h1>My first react app</h1>
+      <Test/>
+    </>
+  )
+}
+
+export default App
---
+++ reactPprojectusingVite/src/Test.jsx
@@ -0,0 +1,9 @@
+
+
+function Testing(){
+    return(
+        <h2>Text</h2>
+    )
+}
+
+export default Testing;
